54088becd46b7e5e4b35ae79168f38bd805a5306
Raheel Dibaji <raheeldibaji1204@gmail.com>
2024-06-05 00:10:51 +0500

    commit

---
+++ basics/02.js
@@ -0,0 +1,14 @@
+let name = "Raheel"
+let id = 1
+let email
+let contact_no = ''
+let age = null
+
+console.table([name, id, typeof name, typeof id, email, typeof email, contact_no, typeof contact_no,
+age, typeof age])
+
+/*
+undefined type is also undefined
+null type is object
+*/
+
